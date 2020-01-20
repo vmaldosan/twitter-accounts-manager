ba8e7dde19cfaf05ffbd94f06c750274fec6550a
Victor Maldonado <vmaldosan@gmail.com>
2020-01-20 23:08:11 +0000

    Copies lists with their members to the destination account.

--- server/routes/lists-routes.js
+++ server/routes/lists-routes.js
@@ -33,63 +33,70 @@ router.get('/list/:type', (req, res) => {
 });
 
 router.post('/list', (req, res) => {
-	console.log(req.body);
 	if (req.body.length === 0) {
 		res.status(422).send('no lists to create');
 	} else {
-		let responseData = req.body.lists.map((list) => {
+		let responseData = req.body.lists.map((origList) => {
+			// Create empty list
 			twitterDest.postCustomApiCall(
-				encodeURI('/lists/create.json?name=' + list.name + '&mode=' + list.mode),
+				encodeURI('/lists/create.json?name=' + origList.name + '&mode=' + origList.mode),
 				{}, error, success);
 
 			function success(data) {
 				const parsed = JSON.parse(data);
-				let filteredData = {
+				let newList = {
 						key: parsed.id_str,
 						name: parsed.name,
 						mode: parsed.mode,
 						membersCount: parsed.member_count
 				};
-				console.log(`list %s created`, filteredData.name);
-				return filteredData;
+				console.log(`list '%s' created`, newList.name);
+
+				// Retrieve members from original list and add them to the new one
+				const membersId = replicateListMembers(origList.key, newList.key);
 			}
 
 			function error(err) {
 				console.log('ERROR [%s]', err);
-				// res.status(500).send(`error while retrieving origin lists: ${body}`);
+				res.status(500).send(`error while retrieving origin lists: ${body}`);
 			};
 		});
-		res.status(200).json(responseData);
 
-		/* const calls = [
-			twitterDest.postCustomApiCall(
-				encodeURI('/lists/create.json?name=' + list.name + '&mode=' + list.mode), 
-				{}, error, success);
-
-			function success(data) {
-				const parsed = JSON.parse(data);
-				let filteredData = {
-						key: parsed.id_str,
-						name: parsed.name,
-						mode: parsed.mode,
-						membersCount: parsed.member_count
-				};
-				console.log(`list %s created`, filteredData.name);
-				return filteredData;
-			}
-
-			function error(err) {
-				console.log('ERROR [%s]', err);
-				// res.status(500).send(`error while retrieving origin lists: ${body}`);
-			}
-		];
-		async.series(tasks, (err, results) => {
-			if (err) {
-				return next(err);
-			}
-			return res.json(results);
-		}); */
+		res.status(204).json(responseData);
 	}
 });
+
+function replicateListMembers(origListId, destListId) {
+	twitterOrig.getCustomApiCall(
+		encodeURI('/lists/members.json?list_id=' + origListId 
+				+ '&cursor=-1&skip_status=true'),
+		{}, error, success);
+
+	function success(data) {
+		const parsed = JSON.parse(data);
+		console.log(`retrieved %d users`, parsed.users.length);
+		const membersId = parsed.users.map(user => user.id_str);
+		addMembers(destListId, membersId);
+	}
+
+	function error(err) {
+		console.log('ERROR [%s]', err);
+	};
+}
+
+function addMembers(listId, usersId) {
+	console.log(usersId.join(','));
+	twitterDest.postCustomApiCall(
+		encodeURI('/lists/members/create_all.json?user_id=' + usersId.join(',') + '&list_id=' + listId),
+		{}, error, success);
+
+	function success() {
+		console.log(`added %d users`, usersId.length);
+	}
+
+	function error(err) {
+		console.log('ERROR [%s]', err);
+	};
+}
 
 module.exports = router;
